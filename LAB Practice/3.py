b42b39d1b87605c59e04f1b4117ea27a1a726932
ParmarDharmrajsinh <parmardharmrajsinh99@gmail.com>
2025-06-16 14:38:20 +0530

    lab code

---
+++ LAB Practice/3.py
@@ -0,0 +1,9 @@
+#program 3:write a python program that demostrates the correct use of indentation ,comments,and variables
+#foloowing PEP 8 guidelines.
+#this is single line comment
+
+'''this is multiple 
+    line comment'''
+a=12
+b=11
+print("the addition is:",a+b)
---
+++ LAB Practice/5.py
@@ -0,0 +1,4 @@
+#program 6:how to create variables in python?
+
+a="this is the way to create varibles"
+print(a)
