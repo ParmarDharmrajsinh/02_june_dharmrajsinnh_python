297bf354c4dd5af759f794a64650308189b1e7bd
ParmarDharmrajsinh <parmardharmrajsinh99@gmail.com>
2025-06-19 16:27:54 +0530

    19

---
+++ assignments/module-1/arbit.py
@@ -0,0 +1,6 @@
+def getdata(data):
+    print("ID:",data[0])
+    print("NAME:",data[1])
+    print ("city:",data[2])
+
+    getdata([101,'parmar','rajkot'])
--- assignments/module-1/for_1.py
+++ assignments/module-1/for_1.py
@@ -1,22 +1,3 @@
 for i in range(1,11):
     print(f"5*{i}={5*i}")
     i+=10
-    no1=int(input("enter number"))
-no2=int(input("enter number2"))
-
-choice=input("enter your choice:")
-
-print("1. circle")
-print("2. rectangle")
-print("3. square")
-
-choice=input("enter your choice:")
-
-if choice==1("area of circle"):
-    print("circle")
-elif choice==2("area of rectangle:"):
-    print("rectangle:")
-elif choice==3("area of square:"):
-    print("square:")
-else:
-    print("another choice")
