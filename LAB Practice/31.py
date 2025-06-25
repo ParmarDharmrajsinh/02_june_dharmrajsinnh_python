45746ec767c7418cf1119a7acb4fe4aa755a2856
ParmarDharmrajsinh <parmardharmrajsinh99@gmail.com>
2025-06-25 15:27:07 +0530

    code

---
+++ LAB Practice/31.py
@@ -0,0 +1,7 @@
+# Get input from user
+user_input = input("Enter a string: ")
+
+# Print the string from the last character using slicing
+reversed_string = user_input[::-1]
+
+print("Reversed string:", reversed_string)
